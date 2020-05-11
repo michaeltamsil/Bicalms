7be1392e07f629f34512c53271a93dc422571734
michael tamsil <michaeltamsil@gmail.com>
2020-05-11 16:00:13 +0700

    Initial Commit

---
+++ bicalms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bicalms
+//
+//  Created by michael tamsil on 11/05/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
