433d076544b68de1ad9967f95f3da514e05cc69d
Michael Tamsil <michaeltamsil@gmail.com>
2020-05-25 19:41:03 +0700

    test full screen

---
+++ bicalms/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  bicalms
+//
+//  Created by michael tamsil on 23/05/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
