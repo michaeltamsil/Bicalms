d17951f8cf4725b4deaee34758121f7c39e6f7b7
Tetsing <iwanggawae@gmail.com>
2020-06-16 09:28:25 +0700

    IE8 done

--- bicalms/Indoor/IndoorExercise8.swift
+++ bicalms/Indoor/IndoorExercise8.swift
@@ -136,7 +136,12 @@ struct IndoorExercise8 : View {
                                             }
                                             
                                         }
-                                        
+                                        .accessibility(label: Text(isPlaying ? "Play": "Pause"))
+                                                                                .accessibility(hint: Text("run animation to show what kind of move"))
+
+
+
+
                                         Image(systemName: "info.circle")
                                             .frame(width: 96)
                                             .font(.largeTitle)
@@ -274,6 +279,9 @@ struct indoorAlertView8 : View {
                         .padding(.top, 8)
                     }
                     
+                    .accessibility(label: Text("Leave"))
+                                      .accessibility(hint: Text("To leave current exercise"))
+                    
                     Button(action: {
                         self.showAlert.toggle()
                     }) {
@@ -284,6 +292,9 @@ struct indoorAlertView8 : View {
                         .frame(width: 120, height: 64)
                         .padding(.top, 8)
                     }
+                    .accessibility(label: Text("Cancel"))
+                                      .accessibility(hint: Text("To continue Squats exercise"))
+                    
                 }
             }
         }
